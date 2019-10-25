02699d9f23a12b22ec6cd6c5235a8d6734209ab9
Faizan Hussain <faizan.hussain@tenpearls.com>
2019-10-25 16:37:44 +0500

    Added review controller

---
+++ Essentials/Controllers/ReviewController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Essentials.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Essentials.Controllers
+{
+    public class ReviewController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<Review> GetAllReviews()
+        {
+            return new List<Review>
+            {
+                new Review
+                {
+                    Title = "ABC",
+                    Description = "A very long description",
+                    Rating = 4,
+                    Author = "John Doe"
+                },
+                new Review
+                {
+                    Title = "XYZ",
+                    Description = "Another very long description",
+                    Rating = 5,
+                    Author = "John Doe"
+                }
+            };
+        }
+    }
+}
---
+++ Essentials/Models/Review.cs
@@ -0,0 +1,11 @@
+﻿namespace Essentials.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Rating { get; set; }
+        public string Author { get; set; }
+    }
+}
